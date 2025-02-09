5f89e8e510560216f2635bd437bfa61fb36bc8b3
nazarein <nazarein@protonmail.com>
2025-02-09 09:56:23 -0500

    Update __init__.py
    Added core.chat import

--- core/__init__.py
+++ core/__init__.py
@@ -2,5 +2,13 @@ from core.twitch_monitor import TwitchMonitor
 from core.wife_alert_monitor import WifeAlertMonitor
 from core.gql_client import GQLClient
 from core.pubsub import Topic, PubSubRequest
+from core.chat import TwitchChat
 
-__all__ = ["TwitchMonitor", "WifeAlertMonitor", "GQLClient", "Topic", "PubSubRequest"]
+__all__ = [
+    "TwitchMonitor",
+    "WifeAlertMonitor",
+    "GQLClient",
+    "Topic",
+    "PubSubRequest",
+    "TwitchChat",
+]
